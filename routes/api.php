8633b26a7625e3b1fbb862fe56b0e1a6ed6f7eef
Helal <freelancerhelaluddin@gmail.com>
2024-07-02 01:15:07 +0600

    update slider api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\ContactController;
+use App\Http\Controllers\Api\ApiController;
 
 Route::get('/posts', [PostController::class, 'getPosts']);
 Route::get('/last-post', [PostController::class, 'getLastPost']);
@@ -19,3 +20,6 @@ Route::get('/product/{id}', [ProductController::class, 'getProductId']);
 
 //contact Email
 Route::post('/contact', [ContactController::class, 'store']);
+
+//slider
+Route::get('/sliders', [ApiController::class, 'slider']);
